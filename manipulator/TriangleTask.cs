952a28d863931f2ba0a468e7f145a20ce57cd255
Eugene Yurchenko <86376359+ruPauk@users.noreply.github.com>
2022-08-25 15:23:48 +0300

    Solved TriangleTask within manipulator project

--- manipulator/TriangleTask.cs
+++ manipulator/TriangleTask.cs
@@ -10,7 +10,13 @@ namespace Manipulation
         /// </summary>
         public static double GetABAngle(double a, double b, double c)
         {
-            return 0.0;
+            if ((a + b >= c) && (a + c >= b) && (b + c >= a))
+            {
+                var cosY = (b * b + a * a - c * c) / (2 * a * b);
+                return Math.Acos(cosY);
+            }
+            else
+                return double.NaN;
         }
     }
 
@@ -19,10 +25,13 @@ namespace Manipulation
     {
         [TestCase(3, 4, 5, Math.PI / 2)]
         [TestCase(1, 1, 1, Math.PI / 3)]
-        // добавьте ещё тестовых случаев!
+        [TestCase(1, 1, 0, 0)]
+        [TestCase(2, 1, 3, Math.PI)]
+        [TestCase(3.0d, 3.0d, 3.0d, 1.0471975511966d)]
         public void TestGetABAngle(double a, double b, double c, double expectedAngle)
         {
-            Assert.Fail("Not implemented yet");
+            var result = TriangleTask.GetABAngle(a, b, c);
+            Assert.AreEqual(expectedAngle, result, 1e-9);
         }
     }
 }
