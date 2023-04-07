c2bb499e029ec586638bdb2e8af5366b8303477e
Eugene Yurchenko <86376359+ruPauk@users.noreply.github.com>
2023-04-07 14:34:03 +0300

    Brainfuck third and the final task is done

--- brainfuck/BrainfuckLoopCommands.cs
+++ brainfuck/BrainfuckLoopCommands.cs
@@ -1,13 +1,47 @@
+using System;
 using System.Collections.Generic;
 
 namespace func.brainfuck
 {
 	public class BrainfuckLoopCommands
 	{
+		private static Dictionary<int, int> _openingBrackets;
+		private static Dictionary<int, int> _closingBrackets;
+		private static void InitializeDictionaries(IVirtualMachine vm)
+		{
+			_openingBrackets = new Dictionary<int, int>();
+			_closingBrackets = new Dictionary<int, int>();
+			var stack = new Stack<int>();
+			for (var i = 0; i < vm.Instructions.Length; i++)
+			{
+				if (vm.Instructions[i] == '[')
+				{
+					stack.Push(i);
+				}
+				else if (vm.Instructions[i] == ']')
+				{
+					var opening = stack.Pop();
+					if (!_openingBrackets.ContainsKey(opening)
+						&& !_closingBrackets.ContainsKey(i))
+					{
+						_openingBrackets.Add(opening, i);
+						_closingBrackets.Add(i, opening);
+					}
+				}
+			}
+		}
+
 		public static void RegisterTo(IVirtualMachine vm)
 		{
-			vm.RegisterCommand('[', b => { });
-			vm.RegisterCommand(']', b => { });
+			InitializeDictionaries(vm);
+			vm.RegisterCommand('[', b => {
+				if (b.Memory[b.MemoryPointer] == 0)
+					b.InstructionPointer = _openingBrackets[b.InstructionPointer];
+			});
+			vm.RegisterCommand(']', b => {
+				if (b.Memory[b.MemoryPointer] > 0)
+					b.InstructionPointer = _closingBrackets[b.InstructionPointer];
+			});
 		}
 	}
 }
