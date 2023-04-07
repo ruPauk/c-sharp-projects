59759877a29c2f572a15c32a21baf7e0da48fafc
Eugene Yurchenko <86376359+ruPauk@users.noreply.github.com>
2023-04-07 17:32:44 +0300

    Rocket first task done

--- rocket/ForcesTask.cs
+++ rocket/ForcesTask.cs
@@ -1,4 +1,8 @@
-﻿namespace func_rocket;
+﻿using System.Drawing;
+using System.Linq;
+using System;
+
+namespace func_rocket;
 
 public class ForcesTask
 {
@@ -6,15 +10,18 @@ public class ForcesTask
 	/// Создает делегат, возвращающий по ракете вектор силы тяги двигателей этой ракеты.
 	/// Сила тяги направлена вдоль ракеты и равна по модулю forceValue.
 	/// </summary>
-	public static RocketForce GetThrustForce(double forceValue) => r => Vector.Zero;
+	public static RocketForce GetThrustForce(double forceValue) => r =>
+		new Vector(forceValue * Math.Cos(r.Direction), forceValue * Math.Sin(r.Direction));
 
 	/// <summary>
 	/// Преобразует делегат силы гравитации, в делегат силы, действующей на ракету
 	/// </summary>
-	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) => r => Vector.Zero;
+	public static RocketForce ConvertGravityToForce(Gravity gravity, Vector spaceSize) =>
+		r => gravity(spaceSize, r.Location);
 
 	/// <summary>
 	/// Суммирует все переданные силы, действующие на ракету, и возвращает суммарную силу.
 	/// </summary>
-	public static RocketForce Sum(params RocketForce[] forces) => forces[0];
+	public static RocketForce Sum(params RocketForce[] forces) => r =>
+		new Vector(forces.Sum(x => x.Invoke(r).X), forces.Sum(x => x.Invoke(r).Y));
 }
