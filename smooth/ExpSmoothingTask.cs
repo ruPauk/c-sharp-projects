05929f1b8faa00b7ec0997305ac4e1ea5f208dfb
Eugene Yurchenko <86376359+ruPauk@users.noreply.github.com>
2023-04-15 09:53:05 +0300

    Added my solution for the first task of Smooth

--- smooth/ExpSmoothingTask.cs
+++ smooth/ExpSmoothingTask.cs
@@ -6,7 +6,20 @@ public static class ExpSmoothingTask
 {
 	public static IEnumerable<DataPoint> SmoothExponentialy(this IEnumerable<DataPoint> data, double alpha)
 	{
-		//Fix me!
-		return data;
+		DataPoint prevPoint = null;
+		foreach (var dataPoint in data)
+		{
+			if (prevPoint == null)
+			{
+				prevPoint = dataPoint.WithExpSmoothedY(dataPoint.OriginalY); ;
+				yield return prevPoint;
+			}
+			else
+			{
+				var tmp = prevPoint.ExpSmoothedY + alpha * (dataPoint.OriginalY - prevPoint.ExpSmoothedY);
+				prevPoint = dataPoint.WithExpSmoothedY(tmp);
+				yield return prevPoint;
+			}
+		}
 	}
 }
