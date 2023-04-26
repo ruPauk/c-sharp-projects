aaeca300f8a3ac772050535f7a6b539e3e65e54b
Eugene Yurchenko <86376359+ruPauk@users.noreply.github.com>
2023-04-26 22:36:27 +0300

    Added not ideal solution for Route-planning task

--- route-planning/PathFinderTask.cs
+++ route-planning/PathFinderTask.cs
@@ -9,7 +9,86 @@ namespace RoutePlanning
 		public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
 		{
 			var bestOrder = MakeTrivialPermutation(checkpoints.Length);
+			int[] workOrder = new int[checkpoints.Length];
+			bestOrder.CopyTo(workOrder, 0);
+			double bestResult = checkpoints.GetPathLength(bestOrder);
+			MixPermutation(workOrder, 0, 0.0, ref bestOrder, bestResult, checkpoints);
 			return bestOrder;
+		}
+
+		private static double AccumulatePathLength(double currentPathLength, Point[] checkpoints,
+			int indexFirst, int indexSecond)
+		{
+			currentPathLength += checkpoints[indexFirst].DistanceTo(checkpoints[indexSecond]);
+			return currentPathLength;
+		}
+
+		//NEED TO FIX "ref int[] bestOrder" and length of this method
+		private static double MixPermutation(int[] curArray, int curElement, double currentPathLength,
+			ref int[] bestOrder, double bestPathLength, Point[] checkpoints)
+		{
+			if (curElement == (curArray.Length - 1))
+			{
+				if (currentPathLength < bestPathLength)
+				{
+					bestOrder = curArray;
+					return currentPathLength;
+				}
+				else
+				{
+					return bestPathLength;
+				}
+			}
+			for (int i = curElement + 1; i < curArray.Length; i++)
+			{
+				var tmpLength = AccumulatePathLength(currentPathLength, checkpoints, curArray[curElement], curArray[i]);
+				if (tmpLength > bestPathLength)
+					return bestPathLength;
+				else
+				{
+					var tmpArray = GetNewIterationArray(curArray, curElement + 1, i);
+					bestPathLength = MixPermutation(tmpArray, curElement + 1, tmpLength, ref bestOrder, bestPathLength, checkpoints);
+				}
+			}
+			return bestPathLength;
+		}
+
+		private static int[] GetNewIterationArray(int[] curArray, int curElement, int chosenElem)
+		{
+			var newCurArray = new int[curArray.Length];
+			curArray.CopyTo(newCurArray, 0);
+			if (curElement == chosenElem)
+			{
+				newCurArray[curElement] = curArray[chosenElem];
+			}
+			else
+			{
+				ExchangeValuesInArray(newCurArray, curElement, chosenElem);
+			}
+			return newCurArray;
+		}
+
+		private static void PrintListOfArrays(List<int[]> arr)
+		{
+			foreach (var elem in arr)
+			{
+				foreach (var i in elem)
+				{
+					Console.Write(i + " ");
+				}
+				Console.WriteLine();
+			}
+		}
+
+		private static void PrintArray(int[] arr, string message, double bestLength, double currentLength)
+		{
+			Console.Write(message + ": ");
+			foreach (var elem in arr)
+			{
+				Console.Write(elem + " ");
+			}
+			Console.Write(" -> best = " + bestLength + " current = " + currentLength);
+			Console.WriteLine();
 		}
 
 		private static int[] MakeTrivialPermutation(int size)
@@ -18,6 +97,20 @@ namespace RoutePlanning
 			for (int i = 0; i < bestOrder.Length; i++)
 				bestOrder[i] = i;
 			return bestOrder;
+		}
+
+		private static void ExchangeValuesInArray(int[] arr, int indexA, int indexB)
+		{
+			int tmp = arr[indexA];
+			arr[indexA] = arr[indexB];
+			arr[indexB] = tmp;
+			return;
+		}
+
+		private static bool CompareDoubles(double bestPathLength, double currentPathLength)
+		{
+			var tmp = currentPathLength - bestPathLength;
+			return tmp > 1e-9;
 		}
 	}
 }
